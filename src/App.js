3ab1620b0fe48a441373baadcf2e16965339d334
chandandeep singh <chandandeep@responseiq.com>
2021-06-10 00:23:19 +0530

    updated auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import AddUser from "./components/Users/AddUsers";
 import UsersList from "./components/Users/UsersList";
-//import Amplify from 'aws-amplify';
-//import awsconfig from './src/aws-exports';
-//import {AmplifySignOut,withAuthenticator} from '@aws-amplify/ui-react';
-//import classes from '../src/components/Users/UsersList.module.css';
-//Amplify.configure(awsconfig);
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut,withAuthenticator} from '@aws-amplify/ui-react';
+import classes from '../src/components/Users/UsersList.module.css';
+Amplify.configure(awsconfig);
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -18,12 +18,12 @@ function App() {
     <div>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-      
+      <AmplifySignOut className = {classes.awsbutton}/>
+
       
     </div>
   );
 }
 
-export default App; 
-//export default withAuthenticator(App); 
-//<AmplifySignOut className = {classes.awsbutton}/>
+
+export default withAuthenticator(App); 
